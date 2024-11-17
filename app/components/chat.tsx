aec38f510777195eae21b588d4a942d667698c21
wyxogo <wyxogo@163.com>
2024-11-17 11:47:40 +0800

    fix

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -474,9 +474,12 @@ export function ChatActions(props: {
   // switch themes
   const theme = config.theme;
   function nextTheme() {
-    const themes = [Theme.Light, Theme.Dark];
+    const themes = [Theme.Auto, Theme.Light, Theme.Dark];
     const themeIndex = themes.indexOf(theme);
     const nextIndex = (themeIndex + 1) % themes.length;
+    if (themes[nextIndex] === Theme.Auto) {
+      nextIndex = (nextIndex + 1) % themes.length;
+    }
     const nextTheme = themes[nextIndex];
     config.update((config) => (config.theme = nextTheme));
   }
