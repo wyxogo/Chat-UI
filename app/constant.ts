db4bd1d190eb3d496d359c5e5d2ba7a541da2dbc
wyxogo <wyxogo@163.com>
2024-11-17 21:22:54 +0800

    Change SAAS_CHAT_URL

--- app/constant.ts
+++ app/constant.ts
@@ -562,5 +562,5 @@ export const PLUGINS = [
   { name: "Search Chat", path: Path.SearchChat },
 ];
 
-export const SAAS_CHAT_URL = "https://nextchat.dev/chat";
-export const SAAS_CHAT_UTM_URL = "https://nextchat.dev/chat?utm=github";
+export const SAAS_CHAT_URL = "https://chat.aifu.tech";
+export const SAAS_CHAT_UTM_URL = "https://chat.aifu.tech?utm=github";
